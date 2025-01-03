16f995a85e93a161aadffd152acce6161c79af36
Amanda Warnakula <amanda@parkwaylabs.com>
2025-01-03 12:01:31 +0530

    first commit

---
+++ models/taskModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  dueDate: {
+    type: Date,
+    required: false,
+  },
+  status: {
+    type: String,
+    default: 'pending',
+  },
+});
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
